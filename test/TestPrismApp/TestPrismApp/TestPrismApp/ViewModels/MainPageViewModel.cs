d9e01f40c1b41f351df117723df0a081a6db9b47
childofbodom24 <waikiki0915_3@yahoo.co.jp>
2019-02-23 23:53:50 +0900

    Added TestPrismApp
    Added TestPrismApp

---
+++ test/TestPrismApp/TestPrismApp/TestPrismApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPrismApp.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private DelegateCommand navigate;
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        public DelegateCommand Navigate =>
+            navigate ?? (navigate = new DelegateCommand(ExecuteNavigateAsync));
+
+        async void ExecuteNavigateAsync()
+        {
+            await this.NavigationService.NavigateAsync("NavigationPage/SubPage");
+        }
+        
+    }
+}
---
+++ test/TestPrismApp/TestPrismApp/TestPrismApp/ViewModels/SubPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPrismApp.ViewModels
+{
+    public class SubPageViewModel : ViewModelBase
+    {
+        public SubPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Sub Page";
+        }
+    }
+}
